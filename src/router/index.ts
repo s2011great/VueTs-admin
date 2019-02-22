4a9c9d0247b7e9fe12779c7ba1212f6acb5b7deb
shanglibo <shanglibo@gbase.cn>
2019-02-22 17:34:08 +0800

    初始化项目配置，并安装所需库。

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -11,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: () => import('@/views/Home.vue'),
     },
     {
       path: '/about',
@@ -19,7 +18,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
     },
   ],
 });
