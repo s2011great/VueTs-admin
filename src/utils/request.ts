f317ba3d7ff4ae2afa67a43ba7c304817697cb3b
shanglibo <shanglibo@gbase.cn>
2019-02-27 14:56:07 +0800

    完成封装axios

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,28 @@
-let service
-export default service = (obj: object) => {
-  return obj
-}
+import axios from 'axios'
+
+const baseURL = ''
+const service = axios.create({
+  baseURL,
+  timeout: 2000
+})
+
+// Request
+service.interceptors.request.use(
+  (confing) => {
+    return confing
+  },
+  (error) => {
+    Promise.reject(error)
+  }
+)
+
+// Response
+service.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    Promise.reject(error)
+  }
+)
+export default service
