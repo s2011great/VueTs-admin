9cc3131008debad1ae7c308cc4ec2190576bab77
shanglibo <shanglibo@gbase.cn>
2019-03-01 10:02:37 +0800

    修复permission bug·

--- src/utils/permission.ts
+++ src/utils/permission.ts
@@ -4,11 +4,15 @@ import 'nprogress/nprogress.css'// progress bar style
 
 router.beforeEach((to, from, next) => {
   NProgress.start()
-  if (to.path !== '/login') {
-    next({path: '/home'})
-    NProgress.done()
-  } else {
+  if (to.path !== '/login' && to.path !== '/home') {
     next({path: '/login'})
     NProgress.done()
+  } else {
+    next()
+    NProgress.done()
   }
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
